4f6dfb30a7be833f4df5ef597205e38d7dc0bb60
Matthew Kewarth <mattkewarth@hotmail.com>
2018-03-04 02:41:25 -0500

    Added persistentserver.c

--- simpleserver.c
+++ simpleserver.c
@@ -24,7 +24,7 @@ int main(int argc, char * argv[]){
 	// some variables
 	int server_fd, client_fd, rc, client_addr_len, opt, rcvd, fd, bytes_read;
 	struct sockaddr_in server_addr, client_addr;
-	char client_msg[1024], *server_msg = "Hello Client\n", *http_root_path, *reqline[3], file_path[1024], data_to_send[1024];
+	char client_msg[1024], *server_msg = "Hello Client\n", *http_root_path, *reqline[5], data_to_send[1024];
 	
 	// requires a port number to listen on and a directory path
 	if(argc != 3){
@@ -56,8 +56,9 @@ int main(int argc, char * argv[]){
 	clean_exit(rc, server_fd, "[Server listen error]: ");
 
 	// Accept connection forever
-	printf("SERVER AT %s:%d LISTENING FOR CONNECTIONS", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
+	printf("SERVER AT %s:%d LISTENING FOR CONNECTIONS\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
 	while(1){
+		char file_path[1024];
 		client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
 		clean_exit(client_fd, server_fd, "[Server accept error]: ");
 		
@@ -67,14 +68,19 @@ int main(int argc, char * argv[]){
 		else if(rcvd == 0) clean_exit(rc, server_fd, "[Client disconnected unexpectedly]: ");
 		else{
 			// successful msg
+			printf("%s\n", client_msg);
 			reqline[0] = strtok (client_msg, " \t\n");
 			// Look if msg was a HTTP GET request
 			if (strncmp(reqline[0], "GET\0", 4) == 0){
 				reqline[1] = strtok(NULL, " \t");
 				reqline[2] = strtok(NULL, " \t\n");
+				reqline[3] = strtok(NULL, "\n");
+				reqline[3] = strtok(NULL, ":\n");
+				
 				// check if msg was using the HTTP 1.0 protocol
 				// we can expand this to include 1.1 later
 				if(strncmp(reqline[2], "HTTP/1.0", 8) != 0){
+					printf("I didnt recognize this!\n");
 					write(client_fd, "HTTP1.0 400 Bad Request\n", 25);
 				}
 				else{
@@ -82,18 +88,36 @@ int main(int argc, char * argv[]){
 					if(strncmp(reqline[1], "/\0" ,2) == 0){
 						reqline[1] = "/index.html";
 					}
-					strcpy(file_path, http_root_path);
-					strcpy(&file_path[strlen(http_root_path)], reqline[1]);
-					printf("file: %s\n", file_path);
-					
-					// File found, send to client
-					if((fd=open(file_path, O_RDONLY)) != -1){
-						send(client_fd, "HTTP/1.0 200 OK\n\n", 17, 0);
-						while((bytes_read=read(fd, data_to_send, 1024)) > 0){
-							write(client_fd, data_to_send, bytes_read);
-						}
+					else if(!(strstr(reqline[1], ".html")) && 
+						!(strstr(reqline[1], ".js")) && 
+						!(strstr(reqline[1], ".css")) && 
+						!(strstr(reqline[1], ".txt")) && 
+						!(strstr(reqline[1], ".jpeg")))
+					{
+						write(client_fd, "HTTP1.0 400 Bad Request\n", 25);
 					}
-					else write(client_fd, "HTTP/1.0 404 Not Found", 23);	
+					else if(strstr(reqline[3], "If-Modified-Since")){
+						//Conditional GET has been found
+						printf("Whoo Woo!\n");
+						
+					}
+					else{
+						
+						
+						strcpy(file_path, http_root_path);
+						strcpy(&file_path[strlen(http_root_path)], reqline[1]);
+						
+						printf("file: %s\n", file_path);
+					
+						// File found, send to client
+						if((fd=open(file_path, O_RDONLY)) != -1){
+							send(client_fd, "HTTP/1.0 200 OK\n\n", 17, 0);
+							while((bytes_read=read(fd, data_to_send, 1024)) > 0){
+								write(client_fd, data_to_send, bytes_read);
+							}
+						}
+						else write(client_fd, "HTTP1.0 404 Not Found\n", 23);	
+					}
 				}
 			}
 			else write(client_fd, "HTTP1.0 400 Bad Request\n", 25);
